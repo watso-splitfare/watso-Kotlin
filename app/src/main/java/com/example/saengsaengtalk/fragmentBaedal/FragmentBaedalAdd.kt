37239525fe5fb9ef9a67bbc8843989446b117631
Titan <tnptt1122@gmail.com>
2022-06-09 02:04:06 +0900

    baedalAdd fragment 추가

--- app/src/main/java/com/example/saengsaengtalk/fragmentBaedal/FragmentBaedalAdd.kt
+++ app/src/main/java/com/example/saengsaengtalk/fragmentBaedal/FragmentBaedalAdd.kt
@@ -1,23 +1,26 @@
 package com.example.saengsaengtalk.fragmentBaedal
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.saengsaengtalk.MainActivity
-import com.example.saengsaengtalk.adapterBaedal.BaedalList
-import com.example.saengsaengtalk.adapterBaedal.BaedalListAdapter
 import com.example.saengsaengtalk.databinding.FragBaedalAddBinding
-import com.example.saengsaengtalk.databinding.FragBaedalListBinding
+import java.text.DecimalFormat
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class FragmentBaedalAdd :Fragment() {
+    //var dateString = ""
+    //var timeString = ""
+    //var orderTime: LocalDateTime? = null
+    var dec = DecimalFormat("00")
 
     private var mBinding: FragBaedalAddBinding? = null
     private val binding get() = mBinding!!
@@ -34,6 +37,43 @@ class FragmentBaedalAdd :Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     fun refreshView() {
         binding.btnPrevious.setOnClickListener { onBackPressed() }
+        val now = LocalDateTime.now()
+        binding.tvOrderTime.text = getDateTimeString(now)
+
+        binding.lytTime.setOnClickListener { showCalendar() }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun showCalendar() {
+        val cal = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            run {
+                var dateString = "${year}-${dec.format(month + 1)}-${dec.format(dayOfMonth)}T"
+
+                val timeSetListener =
+                    TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                        run {
+                            var timeString = "${dec.format(hourOfDay)}:${dec.format(minute)}:00"
+                            var orderTime = LocalDateTime.parse(dateString+timeString)
+                            binding.tvOrderTime.text = getDateTimeString(orderTime)
+                        }
+                    }
+
+                TimePickerDialog(requireContext(), timeSetListener,
+                    cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show()
+            }
+        }
+
+        val dpd = DatePickerDialog(requireContext(), dateSetListener,
+            cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
+        dpd.datePicker.minDate = System.currentTimeMillis() - 1000;
+        dpd.show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getDateTimeString(dateTime: LocalDateTime): String {
+        return dateTime.format(DateTimeFormatter.ofPattern(
+            "MM/dd(E) HH:mm").withLocale(Locale.forLanguageTag("ko")))
     }
 
     fun setFrag(fragment: Fragment, arguments: Map<String, String>? = null) {
