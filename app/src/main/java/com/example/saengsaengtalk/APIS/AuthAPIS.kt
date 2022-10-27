f1cf562b391246fa03ab1d3b5eb0b131c77ebf21
Titan <tnptt1122@gmail.com>
2022-10-28 03:48:20 +0900

    API 인터페이스 분리

---
+++ app/src/main/java/com/example/saengsaengtalk/APIS/AuthAPIS.kt
@@ -0,0 +1,39 @@
+package com.example.saengsaengtalk.APIS
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface AuthAPIS {
+    /** 계정 관련 API */
+
+    @GET("auth/overlap/username")     // 100 아이디 중복조회
+    fun usernameOverlapCheck(
+        @Query("user_name") username: String
+    ): Call<OverlapResult>
+
+    @GET("auth/overlap/studentnum")   // 101 학번 중복조회
+    fun studentnumOverlapCheck(
+        @Query("studentnum") studentnum: Int
+    ): Call<OverlapResult>
+
+    @GET("auth/overlap/nickname")     // 102 닉네임 중복조회
+    fun nicknameOverlapCheck(
+        @Query("nickname") nickname: String
+    ): Call<OverlapResult>
+
+    @POST("auth/signup")                    // 103 회원가입
+    @Headers("accept: application/json", "content-type: application/json")
+    fun signup(
+        @Body jsonparams: SignUpModel
+    ): Call<SignUpResult>
+
+    @POST("auth/login")                     // 104 로그인
+    @Headers("accept: application/json", "content-type: application/json")
+    fun login(
+        @Body jsonparams: LoginModel
+    ): Call<LoginResult>
+
+    @POST("auth/logout")                    // 105 로그아웃
+    fun logout(
+    ): Call<LogoutResult>
+}
---
+++ app/src/main/java/com/example/saengsaengtalk/APIS/BaedalAPIS.kt
@@ -0,0 +1,69 @@
+package com.example.saengsaengtalk.APIS
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface BaedalAPIS {
+    /** 배달 게시물 관련 api */
+
+    @GET("order/store/list")                // 201 가게 리스트 조회
+    fun getStoreList(): Call<List<StoreListModel>>
+
+    @GET("order/menu/list")                 // 202 메뉴 조회
+    fun getSectionMenu(
+        @Query("store_id") storeId: String
+    ): Call<List<SectionMenuModel>>
+
+    @GET("order/menu/detail")               // 203 옵션 조회
+    fun getGroupOption(
+        //@Query("menu_id") menuId: Int,      // 테스트용(실제 사용 x)
+        @Query("menu_name") menuName: String,
+        @Query("store_id") storeId: String
+    ): Call<List<GroupOptionModel>>
+
+    @POST("order/post/posting")             // 204 배달 게시글 등록
+    fun baedalPosting(
+        @Body jsonparams: BaedalPostingModel
+    ): Call<BaedalPostingResponse>
+
+    @GET("order/post/detail/{post_id}")     // 205 배달 게시글 조회
+    fun getBaedalPost(
+        @Path("post_id") postId: String
+        //@Query("post_id") postId: Int,
+    ): Call<BaedalPostModel>
+
+    @PATCH("order/post/update")             // 206 배달 게시글 수정
+    fun updateBaedalPost(
+        @Body jsonparams: BaedalUpdateModel
+    ): Call<BaedalPostingResponse>
+
+    @PATCH("order/post/condition-switch")    // 207 배달 주문 가능 여부 변경
+    fun setClosed(
+        @Body jsonparams: Map<String, String>
+    ): Call<BaedalConditionResponse>
+
+    @POST("order/ordering")                 // 209 배달 주문 등록
+    fun baedalOrdering(
+        @Body jsonparams: OrderingModel
+    ): Call<OrderingResponse>
+
+    @GET("/order/ordering/update")          // 210 배달 주문 수정용 데이터 요청
+    fun getOrders(
+        @Query("post_id") postId: String
+    ): Call<UserOrder>
+
+    @PATCH("order/ordering/update")         // 210 배달 주문 수정
+    fun baedalOrderUpdate(
+        @Body jsonparams: OrderingModel
+    ): Call<OrderingResponse>
+
+    @GET("order/post/join/condition-switch")// 211 배달 주문 취소
+    fun baedalOrderCancel(
+        @Query("post_id") postId: String,
+    ): Call<BaedalPostingResponse>
+
+    @GET("order/post/list")                 // 213 배달 게시글 리스트 조회
+    fun getBaedalOrderListPreview(
+        //@Query("from_index") fromIndex: Int,
+    ): Call<List<BaedalPostPreviewModel>>
+}
