6f57a95338bf392af3f538e835c5b87eb65299c7
JH <tnptt1122@gmail.com>
2023-05-02 22:44:36 +0900

    feat: 상태코드 401 예외 처리
    특정 도메인에 요청하는 API는 응답 상태가 401이더라도 토큰 재발급 요청하지 않음

--- app/src/main/java/com/saengsaengtalk/app/API/API.kt
+++ app/src/main/java/com/saengsaengtalk/app/API/API.kt
@@ -3,7 +3,6 @@ import com.saengsaengtalk.app.API.*
 import com.saengsaengtalk.app.MainActivity
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.saengsaengtalk.app.API.DataModels.ErrorResponse
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -19,7 +18,12 @@ interface API:AuthAPI, UserAPI, BaedalAPI, TaxiAPIS, AdminAPIS {
     companion object {
         //private const val BASE_URL = "https://24489c78-e8fa-4f59-9466-05c9d568ce74.mock.pstmn.io/"
         private const val BASE_URL = "http://52.78.106.235/api/"
-
+        private val EXCEPTION_URL = listOf(
+            "${BASE_URL}auth/login",
+            "${BASE_URL}user/signup",
+            "${BASE_URL}user/signup/validation-check",
+            "${BASE_URL}user/forogt/password"
+        )
         fun create(): API {
             val gson :Gson = GsonBuilder().setLenient().create();
 
@@ -41,56 +45,44 @@ interface API:AuthAPI, UserAPI, BaedalAPI, TaxiAPIS, AdminAPIS {
         class AppInterceptor : Interceptor { @Throws(IOException::class)
 
             override fun intercept(chain: Interceptor.Chain): Response {
-            val prefs = MainActivity.prefs
+                val prefs = MainActivity.prefs
                 val accessToken = prefs.getString("accessToken", "")
                 val refreshToken = prefs.getString("refreshToken", "")
+
+                val targetUrl = chain.request().url().toString()
+
                 val tokenAddedRequest = chain.request().newBuilder()
                     .addHeader("Authorization", accessToken)
                     .build()
 
                 val response = chain.proceed(tokenAddedRequest)
 
-                Log.d("API intercept response", response.toString())
-                Log.d("API intercept response.code", response.code().toString())
-
-                if (response.code() == 401) {
-                    val body = response.body()?.string()!!
-                    val gson = Gson()
-                    val errorResponse = gson.fromJson(body, ErrorResponse::class.java)
-                    Log.e("API errorResponse.msg", errorResponse.msg)
-                    Log.e("API errorResponse.code", errorResponse.code.toString())
+                if (response.code() == 401 && (targetUrl !in EXCEPTION_URL)) {
                     try {
-                        if (errorResponse.code == 202) {
-                            response.close()
-                            Log.d("어세스 토큰 갱신 시도 access", accessToken)
-                            Log.d("어세스 토큰 갱신 시도 refresh", refreshToken)
-                            val refreshRequest = chain.request().newBuilder()
-                                .addHeader("Authorization", refreshToken)
-                                .method("GET", null)
-                                .url(BASE_URL + "auth/refresh")
-                                .build()
+                        Log.d("API", "토큰 만료")
+                        response.close()
+                        Log.d("어세스 토큰 갱신 시도 access", accessToken)
+                        Log.d("어세스 토큰 갱신 시도 refresh", refreshToken)
+                        val refreshRequest = chain.request().newBuilder()
+                            .addHeader("Authorization", refreshToken)
+                            .method("GET", null)
+                            .url(BASE_URL + "auth/refresh")
+                            .build()
 
-                            val refreshResponse = chain.proceed(refreshRequest)
-                            val token = refreshResponse.headers().get("Authentication").toString()
-                            prefs.setString("accessToken", token)
+                        val refreshResponse = chain.proceed(refreshRequest)
+                        val token = refreshResponse.headers().get("Authentication").toString()
+                        prefs.setString("accessToken", token)
 
-                            Log.d("어세스 토큰 갱신 성공", token)
-                            val newRequest = chain.request().newBuilder()
-                                .addHeader("Authorization", token)
-                                .build()
+                        Log.d("어세스 토큰 갱신 성공", token)
+                        val newRequest = chain.request().newBuilder()
+                            .addHeader("Authorization", token)
+                            .build()
 
-                            val newRes = chain.proceed(newRequest)
-                            Log.d("API intercept newRes", newRes.toString())
-                            Log.d("API intercept newRes.code", newRes.code().toString())
-                            return newRes
-                        } else {
-                            prefs.removeString("accessToken")
-                            prefs.removeString("refreshToken")
-                            prefs.removeString("userId")
-                            prefs.removeString("nickname")
-                        }
-                    }
-                    catch(e:Exception) { }
+                        val newRes = chain.proceed(newRequest)
+                        Log.d("API intercept newRes", newRes.toString())
+                        Log.d("API intercept newRes.code", newRes.code().toString())
+                        return newRes
+                    } catch (e: Exception) { }
                     finally { }
                 }
                 return response
