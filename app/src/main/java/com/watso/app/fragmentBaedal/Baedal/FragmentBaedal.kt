807c19424b45f78c206d27c038db166577a68a7c
JH <tnptt1122@gmail.com>
2023-05-04 09:21:32 +0900

    style: 홈 화면 히스토리 버튼 추가

--- app/src/main/java/com/watso/app/fragmentBaedal/Baedal/FragmentBaedal.kt
+++ app/src/main/java/com/watso/app/fragmentBaedal/Baedal/FragmentBaedal.kt
@@ -38,7 +38,7 @@ class FragmentBaedal :Fragment() {
         mBinding = FragBaedalBinding.inflate(inflater, container, false)
 
         binding.btnOption.setOnClickListener { setFrag(FragmentAccount(), fragIndex=0) }
-        binding.tvBaedalHistory.setOnClickListener { setFrag(FragmentBaedalHistory()) }
+        binding.btnBaedalHistory.setOnClickListener { setFrag(FragmentBaedalHistory()) }
         binding.btnBaedalPostAdd.setOnClickListener { setFrag(FragmentBaedalAdd()) }
         binding.scrollView.setOnTouchListener { _, event -> isTouched = when (event.action)
             {
