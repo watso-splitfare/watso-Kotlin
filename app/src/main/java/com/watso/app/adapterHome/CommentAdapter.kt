59f389e017f8c46adab65118409c51b7784d11d8
JH <tnptt1122@gmail.com>
2023-05-08 20:01:52 +0900

    style: 댓글 버튼 디자인

--- app/src/main/java/com/watso/app/adapterHome/CommentAdapter.kt
+++ app/src/main/java/com/watso/app/adapterHome/CommentAdapter.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.watso.app.API.BaedalPost
 import com.watso.app.API.Comment
 import com.watso.app.API.VoidResponse
+import com.watso.app.R
 import com.watso.app.databinding.LytCommentBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -54,36 +55,55 @@ class CommentAdapter(val context: Context, val comments: MutableList<Comment>, v
         @RequiresApi(Build.VERSION_CODES.O)
         fun bind(comment: Comment) {
             if (adapterPosition == 0) binding.divider.visibility = View.GONE
-            if (comment.userId == userId) {
-                binding.btnDelete.setOnClickListener {
-                    val builder = AlertDialog.Builder(context)
-                    builder.setTitle("댓글 삭제하기")
-                        .setMessage("댓글을 삭제하시겠습니까?")
-                        .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, id ->
-                            deleteComment(comment)  })
-                        .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, id -> })
-                    builder.show()
-                }
-            } else binding.btnDelete.visibility = View.GONE
-            binding.btnReply.setOnClickListener {
-                replyListener.makeReply(comment)
-            }
-
-            if (comment.parentId == null) binding.ivReply.visibility = View.GONE
-            else binding.btnReply.visibility = View.GONE
+            /**
+             * 부모댓글 일 때
+             *      내 댓글         -> 답글, 삭제
+             *      내 댓글 아님     -> 답글
+             * 대댓글 일 때
+             *      내 댓글         -> 삭제
+             *      내 댓글 아님     ->
+             *
+             */
 
             if (comment.status == "created") {
                 binding.tvNickname.text = comment.nickname
                 binding.tvContent.text = comment.content
                 val createdAt = LocalDateTime.parse(comment.createdAt, DateTimeFormatter.ISO_DATE_TIME)
                 binding.tvCreatedAt.text = createdAt.format(DateTimeFormatter.ofPattern("MM/dd HH:mm"))
-            } else {
+                binding.btnReply.setOnClickListener { replyListener.makeReply(comment) }
+                binding.btnDelete.setOnClickListener { alertDelete(comment) }
+
+                if (comment.parentId == null) {         // 부모 댓글일 때
+                    binding.ivReply.visibility = View.GONE
+                    if (comment.userId != userId) {                 // 내 댓글이 아니면
+                        binding.btnDelete.visibility = View.GONE
+                        binding.btnReply.setBackgroundResource(R.drawable.btn_area_alone)
+                        binding.divCommentArea.visibility = View.GONE
+                    }
+                } else {                                // 대댓글일 때
+                    binding.btnReply.visibility = View.GONE
+                    binding.divCommentArea.visibility = View.GONE
+                    if (comment.userId == userId ) {                // 내 댓글이면
+                        binding.btnDelete.setBackgroundResource(R.drawable.btn_area_alone)
+                    } else binding.btnDelete.visibility = View.GONE // 내 댓글이 아니면
+                }
+            } else {        // 삭제된 댓글
                 binding.tvNickname.visibility = View.GONE
-                binding.tvCreatedAt.visibility = View.GONE
                 binding.tvContent.text = "삭제된 댓글입니다."
+                binding.tvCreatedAt.visibility = View.GONE
                 binding.btnDelete.visibility = View.GONE
                 binding.btnReply.visibility = View.GONE
             }
+        }
+
+        fun alertDelete(comment: Comment) {
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("댓글 삭제하기")
+                .setMessage("댓글을 삭제하시겠습니까?")
+                .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, id ->
+                    deleteComment(comment)  })
+                .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, id -> })
+            builder.show()
         }
 
         fun deleteComment(comment: Comment) {
