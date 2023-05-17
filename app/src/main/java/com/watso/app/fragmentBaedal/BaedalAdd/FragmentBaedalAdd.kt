fcb1eda71fe03c5e5cb7b28cce0f796d019af259
JH <tnptt1122@gmail.com>
2023-05-18 02:57:43 +0900

    fix: 시계 오전, 오후 잘리던 문제 임시 해결
    폰트 문제였음.. 너무 안이쁘니까 커스텀 테마 만들자

--- app/src/main/java/com/watso/app/fragmentBaedal/BaedalAdd/FragmentBaedalAdd.kt
+++ app/src/main/java/com/watso/app/fragmentBaedal/BaedalAdd/FragmentBaedalAdd.kt
@@ -152,6 +152,8 @@ class FragmentBaedalAdd :Fragment(), View.OnTouchListener {
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun showCalendar() {
+        val themeResId = com.google.android.material.R.style.Widget_Material3_MaterialTimePicker
+
         val cal = Calendar.getInstance()
         val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
             run {
@@ -166,12 +168,12 @@ class FragmentBaedalAdd :Fragment(), View.OnTouchListener {
                         }
                     }
 
-                TimePickerDialog(requireContext(), timeSetListener,
+                TimePickerDialog(requireContext(), themeResId, timeSetListener,
                     cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show()
             }
         }
 
-        val dpd = DatePickerDialog(requireContext(), dateSetListener,
+        val dpd = DatePickerDialog(requireContext(), themeResId, dateSetListener,
             cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
         dpd.datePicker.minDate = System.currentTimeMillis() - 1000
         dpd.datePicker.maxDate = System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7
